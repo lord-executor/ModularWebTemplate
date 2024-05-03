baacc47d6427ffce6b02c59aefc27b631b70f496
Lukas Angerer <executor.dev@gmail.com>
2024-05-03 16:20:48 +0200

    Fixed user profile path for config loading

--- WebTemplate/AppServer.cs
+++ WebTemplate/AppServer.cs
@@ -61,6 +61,6 @@ public class AppServer
 
     private void SetupConfiguration(ConfigurationManager configuration, IWebHostEnvironment env)
     {
-        configuration.AddJsonFile($"~/{env.ApplicationName}.json", optional: true);
+        configuration.AddJsonFile($"{Environment.GetFolderPath(Environment.SpecialFolder.UserProfile)}/{env.ApplicationName}.json", optional: true);
     }
 }
