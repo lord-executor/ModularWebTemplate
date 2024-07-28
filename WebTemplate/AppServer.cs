01e563f18cec98f6cb2c9dd6fd2e0b5e9ae6c255
Lukas Angerer <executor.dev@gmail.com>
2024-07-28 13:37:30 +0200

    Command line argument for version output

--- WebTemplate/AppServer.cs
+++ WebTemplate/AppServer.cs
@@ -11,26 +11,44 @@ namespace WebTemplate;
 
 public class AppServer
 {
+    private readonly IVersionInfo _versionInfo;
+
     /// <summary>
     /// These are all the modules that are loaded. The order is _somewhat_ important since in many situations it is
     /// relevant which configuration or middleware is applied first and the modules are applied in the order in
     /// which they appear here.
     /// </summary>
-    private readonly IList<IAppConfigurationModule> _modules = new List<IAppConfigurationModule>
+    private readonly IList<IAppConfigurationModule> _modules;
+
+    public AppServer(IVersionInfo versionInfo)
     {
-        new AuthModule(),
-        new CorsModule(),
-        new JsonModule(),
-        new SwaggerModule(),
-        new ValidationModule(),
-        new StatusEndpointModule(_ => new GitVersionInfo()),
-        new ControllersModule(),
-        // Keep the SpaRoutingModule at the very end
-        new SpaRoutingModule(),
-    };
+        _versionInfo = versionInfo;
+        _modules = new List<IAppConfigurationModule>
+        {
+            new AuthModule(),
+            new CorsModule(),
+            new JsonModule(),
+            new SwaggerModule(),
+            new ValidationModule(),
+            new StatusEndpointModule(_ => _versionInfo),
+            new ControllersModule(),
+            // Keep the SpaRoutingModule at the very end
+            new SpaRoutingModule(),
+        };
+    }
 
     public void Start(string[] args)
     {
+        if (args.Contains("--version"))
+        {
+            Console.WriteLine(string.IsNullOrEmpty(_versionInfo.Version)
+                // no tags available yet, so we fall back to the assembly version
+                ? _versionInfo.AssemblyVersion
+                : _versionInfo.Version);
+
+            return;
+        }
+
         var builder = WebApplication.CreateBuilder(args);
         SetupConfiguration(builder.Configuration, builder.Environment);
 
--- WebTemplate/Program.cs
+++ WebTemplate/Program.cs
@@ -3,5 +3,5 @@ using WebTemplate;
 // After launching the application, you can access
 // * https://localhost:7169/swagger/
 // * https://localhost:7169/v1/status
-var server = new AppServer();
+var server = new AppServer(new GitVersionInfo());
 server.Start(args);
